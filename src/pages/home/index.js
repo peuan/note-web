c7d12e36058c940b09ab7eb8e620e70373f3d718
byRockstar <ballzznatthaphon@hotmial.com>
2020-11-20 00:11:03 +0700

    edit Home to School in Home Page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,11 +2,11 @@ import Layout from "../../layout";
 const Home = () => {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>School</h1>
       <h2>Home</h2>
       <h3>Home</h3>
       <h4>Home</h4>
-      <h5>Home</h5>
+      <h5>School</h5>
     </Layout>
   );
 };
