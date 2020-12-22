82979e0a68c1ccb3af511897dbc236098d147b05
Natthaphon Srikong <ballzznatthaphon@hotmail.com>
2020-12-22 23:25:24 +0700

    edit like public note

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,4 +1,4 @@
-import { Col, Row, Skeleton } from "antd";
+import { Row, Skeleton } from "antd";
 import Layout from "antd/lib/layout/layout";
 import { useEffect, useState } from "react";
 import { PublicNotesService } from "../../services/public-note";
@@ -7,7 +7,8 @@ import CardHome from "./card";
 const ViewHome = () => {
   const [publicNotes, setPublicNote] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [like, setLike] = useState(false);
+  const [userLiked, setUserLiked] = useState([]);
+  const [loadingUserLiked, setLoadingUserLiked] = useState(true);
 
   useEffect(() => {
     getNotes();
@@ -21,9 +22,28 @@ const ViewHome = () => {
     setIsLoading(false);
   };
 
-  const likeNote = async () => {};
+  const updateLike = async (noteId, isLiked) => {
+    const index = publicNotes.findIndex((note) => note.id === noteId);
+    const newPublicNotes = [...publicNotes];
+    const note = newPublicNotes[index];
+    note.isLiked = isLiked;
+    if (isLiked) {
+      note.totalLike = note.totalLike + 1;
+    } else {
+      note.totalLike = note.totalLike - 1;
+    }
+    setPublicNote(newPublicNotes);
+    if (isLiked) {
+      await PublicNotesService.updateLike(noteId);
+    } else {
+      await PublicNotesService.updateDislike(noteId);
+    }
+  };
 
-  const unlinkNote = async () => {};
+  const usersLiked = async (noteId) => {
+    setLoadingUserLiked(true);
+    const users = await PublicNotesService.getUsersLike(noteId);
+  };
 
   return (
     <Layout>
@@ -32,7 +52,13 @@ const ViewHome = () => {
           <Skeleton active />
         ) : (
           publicNotes.map((note) => {
-            return <CardHome note={note} likeNote={likeNote} />;
+            return (
+              <CardHome
+                note={note}
+                usersLiked={usersLiked}
+                updateLike={updateLike}
+              />
+            );
           })
         )}
       </Row>
